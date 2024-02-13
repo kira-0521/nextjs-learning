2df6d114a60244d1fdcc18e29535a7caea219704
kira-0521 <kiratanaka0521@gmail.com>
2024-02-13 19:16:37 +0900

    error and notfound

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,16 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
+    fetchInvoiceById('2e94d1ed-d220-449f-9f11-f0bbceed9645'),
     fetchCustomers(),
   ]);
+
+  if (!invoice) notFound();
 
   return (
     <main>
